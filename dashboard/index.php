74139eca6feab715fc0cc06d22d4761b1ac41861
AfifJ <110212622+AfifJ@users.noreply.github.com>
2023-05-29 00:28:46 +0700

    litle improvement

--- dashboard/index.php
+++ dashboard/index.php
@@ -20,22 +20,20 @@ $file = $conn->query("select * from news inner join publish on news.id = publish
     <div class="grid place-items-center mr-6 ml-6">
       <div class="w-full mx-auto my-6 grid gap-4 max-w-6xl place-items-center">
 
-        <a href="input.php"
-          class="flex items-center justify-center h-44 w-full md:max-w-xl bg-gray-100 rounded-lg hover:bg-gray-300">
+        <a href="input.php" class="flex items-center justify-center h-44 w-full md:max-w-xl bg-gray-100 rounded-lg hover:bg-gray-300">
           <p class="text-2xl text-gray-400">+</p>
         </a>
 
         <?php
         while ($article = $file->fetch_object()) {
+          $id = $article->id;
           $title = $article->title;
           $image = $article->image;
           $desc = $article->content;
-          ?>
-          <div 
-            class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 cursor-pointer">
+        ?>
+          <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 cursor-pointer">
             <?php if (!empty($image)) { ?>
-              <img class="md:w-48 h-full md:aspect-square object-cover rounded-t-lg md:rounded-none md:rounded-l-lg"
-                src="<?= $image ?>" alt="">
+              <img class="md:w-48 h-full md:aspect-square object-cover rounded-t-lg md:rounded-none md:rounded-l-lg" src="<?= $image ?>" alt="">
             <?php } ?>
             <div class="flex flex-col justify-between p-4 leading-normal overflow-hidden">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 line-clamp-2">
@@ -45,43 +43,54 @@ $file = $conn->query("select * from news inner join publish on news.id = publish
                 <?= $desc ?>
               </p>
             </div>
-
-            <button id="dropdownMenuIconHorizontalButton" data-dropdown-toggle="dropdownDotsHorizontal"
-              class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white bg-opacity-0 rounded-lg hover:bg-gray-100 focus:outline-none"
-              type="button">
-              <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg">
-                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z">
-                </path>
-              </svg>
-            </button>
-
-            <!-- Dropdown menu -->
-            <div id="dropdownDotsHorizontal"
-              class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
-              <ul class="py-2 text-sm text-gray-700"
-                aria-labelledby="dropdownMenuIconHorizontalButton">
-                <li>
-                  <a href="#"
-                    class="block px-4 py-2 hover:bg-gray-100">Dashboard</a>
+            <div class="h-full p-4">
+              <ul class="py-2 text-sm text-gray-700">
+                <li class="">
+                  <form action="update.php" method="post">
+                    <input type="hidden" name="newsid" value="<?= $article->id
+                                                              ?>">
+                    <button type="submit" class="block px-4 py-2 hover:bg-gray-200" name="update">
+                      Update
+                    </button>
+                  </form>
                 </li>
                 <li>
-                  <a href="#"
-                    class="block px-4 py-2 hover:bg-gray-100-600">Settings</a>
-                </li>
-                <li>
-                  <a href="#"
-                    class="block px-4 py-2 hover:bg-gray-100">Earnings</a>
+                  <form method="post">
+                    <input type="hidden" name="newsid" value="<?= $article->id
+                                                              ?>">
+                    <button type="submit" class="block px-4 py-2 hover:bg-gray-200" name="askdelete">
+                      Delete
+                    </button>
+                  </form>
+                  <?php if (isset($_POST['askdelete'])) {
+                  ?>
+                    <div class="fixed top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full bg-gray-500 bg-opacity-80">
+                      <div class="relative w-full max-w-md max-h-full m-auto">
+                        <div class="relative bg-white rounded-lg shadow">
+                          <div class="p-6 text-center">
+                            <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                            </svg>
+                            <h3 class="mb-5 text-lg font-normal text-gray-500">Are you sure you want to delete this product?</h3>
+                            <form method="post">
+                              <button type="submit" data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                Yes, I'm sure
+                              </button>
+                              <button type="submit" data-modal-hide="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">No, cancel</button>
+                            </form>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  <?php
+                  } ?>
                 </li>
               </ul>
-              <div class="py-2">
-                <a href="#"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Separated
-                  link</a>
-              </div>
+
+
             </div>
           </div>
-          <?php
+        <?php
         }
         ?>
 
--- dashboard/update.php
+++ dashboard/update.php
@@ -26,37 +26,61 @@ include "inc/navbar.php";
 
 <div class="p-4 sm:ml-64">
   <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg mt-14">
+    <?php if (!isset($_POST['update'])) { ?>
+      <form method="post">
+        <div class="mb-6">
+          <label for="newsid" class="block mb-2 text-sm font-medium text-gray-900">News ID</label>
+          <select name="newsid" id="newsid" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <?php $q = "SELECT id FROM news";
+            $results = $conn->query($q);
+            while ($result = $results->fetch_object()) {
+            ?>
+              <option value="<?= $result->id ?>" default><?= $result->id ?></option>
+            <?php
+            }
+            ?>
+          </select>
+        </div>
+        <div class="mb-6">
+          <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" name="update">Update</button>
+        </div>
+      </form>
+    <?php } ?>
+    <?php
+    if (isset($_POST['update'])) {
+      $NewsID = $_POST['newsid'];
+    ?>
+      <form method="post">
+        <div class="mb-6">
+          <label for="newsid" class="block mb-2 text-sm font-medium text-gray-900">News id</label>
+          <input type="text" id="newsid" name="id" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value="<?= $NewsID ?>" disabled>
+        </div>
+        <?php
 
-    <form method="post">
-      <div class="mb-6">
-        <label for="newsid" class="block mb-2 text-sm font-medium text-gray-900">News ID</label>
-        <select name="newsid" id="newsid"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-          <option value="1" default>1</option>
-          <option value="2">2</option>
-        </select>
-      </div>
-      <div class="mb-6">
-        <label for="title" class="block mb-2 text-sm font-medium text-gray-900">Your title</label>
-        <input type="text" id="title" name="title"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          placeholder="" required>
-      </div>
-      <div class="mb-6">
-        <label for="image" class="block mb-2 text-sm font-medium text-gray-900">Your image link</label>
-        <input type="text" id="image" name="image"
-          class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          placeholder="https://www...">
-      </div>
-      <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Content</label>
-      <textarea id="message" rows="4" name="content"
-        class="block mb-6 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-        placeholder="" required></textarea>
-      <button type="submit" name="submit"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Publish</button>
+        $q = "SELECT title, content, image FROM news WHERE id = '$NewsID'";
+        $result = $conn->query($q)->fetch_object();
+        $title = $result->title;
+        $image = $result->image;
+        $content = $result->content;
+        ?>
+        <div class="mb-6">
+          <label for="title" class="block mb-2 text-sm font-medium text-gray-900">Your title</label>
+          <input type="text" id="title" name="title" value="<?= $title ?>" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="" required>
+        </div>
+        <div class="mb-6">
+          <label for="image" class="block mb-2 text-sm font-medium text-gray-900">Your image link</label>
+          <input type="text" id="image" name="image" value="<?= $image ?>" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="https://www...">
+        </div>
+        <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Content</label>
+        <textarea id="message" rows="4" name="content" class="block mb-6 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="" required><?= $content ?></textarea>
+        <button type="submit" name="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Publish</button>
 
 
-    </form>
+      </form>
+    <?php
+    }
+
+    ?>
 
 
 
